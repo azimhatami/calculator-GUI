abd31cf0022468450d17afaeb4296441a5bc6d10
Azim Hatami <azimhatami4@gmail.com>
2023-12-18 14:46:39 -0500

    Completion of the project

--- main.py
+++ main.py
@@ -2,176 +2,129 @@ import tkinter as tk
 
 window = tk.Tk()
 
-lbl_screen = tk.Label(
-    master=window,
-    # width=50,
-    height=4,
+window.title('Calculator')
+
+lbl_calc_result = tk.Label(
+    master = window,
+    text = '0',
+    width = 30,
+    height = 3,
 )
 
+lbl_calc_result.grid(row=0, column=0, columnspan=4)
 
-btn_seven = tk.Button(
-    master=window,
-    text='7',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('7')
-)
+last_op_index = -1
+last_dot_index = -1
 
-btn_eight = tk.Button(
-    master=window,
-    text='8',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('8')
-)
+def insert_number_in_calc_result(btn_text):
+    current = lbl_calc_result['text']
 
-btn_nine = tk.Button(
-    master=window,
-    text='9',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('9')
-)
+    global last_op_index, last_dot_index
+    if btn_text in ['+', '-', '*']:
+        last_op_index = len(current)
 
-btn_plus = tk.Button(
-    master=window,
-    text='+',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('+')
-)
+    if btn_text == 'C':
+        lbl_calc_result['text'] = '0'
+        last_op_index, last_dot_index = 0, 0
+    elif current == '0':
+        lbl_calc_result['text'] = btn_text
+    elif btn_text == '=':
+        result = f'{eval(current)}'
+        lbl_calc_result['text'] = result
+        last_op_index, last_dot_index = 0, 0
+        if '.' in result:
+            last_dot_index = result.index('.')
+    # else:
+    elif btn_text == '.' and not (last_dot_index > last_op_index or
+                                  current[-1] == '.'):
+            lbl_calc_result['text'] += btn_text
+            last_dot_index = len(current)
 
-btn_four = tk.Button(
-    master=window,
-    text='4',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('4')
-)
+    elif btn_text in ['+', '-', '*'] and current[-1] in ['+', '-', '*']:
+        lbl_calc_result['text'] = current[:-1] + btn_text
+    else:
+        lbl_calc_result['text'] += btn_text
 
-btn_five = tk.Button(
-    master=window,
-    text='5',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('5')
-)
+calc_keys_objs = []
 
-btn_six = tk.Button(
-    master=window,
-    text='6',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('6')
-)
+calc_keys = [
+    {
+        'text': '7',
+        'command': lambda: insert_number_in_calc_result('7'),
+    },
+    {
+        'text': '8',
+        'command': lambda: insert_number_in_calc_result('8'),
+    },
+    {
+        'text': '9',
+        'command': lambda: insert_number_in_calc_result('9'),
+    },
+    {
+        'text': '+',
+        'command': lambda: insert_number_in_calc_result('+'),
+    },
+    {
+        'text': '4',
+        'command': lambda: insert_number_in_calc_result('4'),
+    },
+    {
+        'text': '5',
+        'command': lambda: insert_number_in_calc_result('5'),
+    },
+    {
+        'text': '6',
+        'command': lambda: insert_number_in_calc_result('6'),
+    },
+    {
+        'text': '-',
+        'command': lambda: insert_number_in_calc_result('-'),
+    },
+    {
+        'text': '1',
+        'command': lambda: insert_number_in_calc_result('1'),
+    },
+    {
+        'text': '2',
+        'command': lambda: insert_number_in_calc_result('2'),
+    },
+    {
+        'text': '3',
+        'command': lambda: insert_number_in_calc_result('3'),
+    },
+    {
+        'text': '*',
+        'command': lambda: insert_number_in_calc_result('*'),
+    },
+    {
+        'text': '.',
+        'command': lambda: insert_number_in_calc_result('.'),
+    },
+    {
+        'text': '0',
+        'command': lambda: insert_number_in_calc_result('0'),
+    },
+    {
+        'text': 'C',
+        'command': lambda: insert_number_in_calc_result('C'),
+    },
+    {
+        'text': '=',
+        'command': lambda: insert_number_in_calc_result('='),
+    },
+]
 
-btn_minus = tk.Button(
-    master=window,
-    text='-',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('-')
-)
+for calc_key_data in calc_keys:
+    btn = tk.Button(
+        master = window,
+        text = calc_key_data['text'],
+        command = calc_key_data['command'],
+        height = 3,
+    )
+    calc_keys_objs.append(btn)
 
-btn_one = tk.Button(
-    master=window,
-    text='1',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('1')
-)
-
-btn_two = tk.Button(
-    master=window,
-    text='2',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('2')
-)
-
-btn_three = tk.Button(
-    master=window,
-    text='3',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('3')
-)
-
-btn_multiply = tk.Button(
-    master=window,
-    text='*',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('*')
-)
-
-btn_dot = tk.Button(
-    master=window,
-    text='.',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('.')
-)
-
-btn_zero = tk.Button(
-    master=window,
-    text='0',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('0')
-)
-
-btn_clear = tk.Button(
-    master=window,
-    text='C',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('C')
-)
-
-btn_equal = tk.Button(
-    master=window,
-    text='=',
-    font = ('Sans','15','bold'),
-    width=3,
-    height=2,
-    command=lambda: print('=')
-)
-
-lbl_screen.grid(row=0, column=0)
-btn_seven.grid(row=1, column=0)
-btn_eight.grid(row=1, column=1)
-btn_nine.grid(row=1, column=2)
-btn_plus.grid(row=1, column=3)
-btn_four.grid(row=2, column=0)
-btn_five.grid(row=2, column=1)
-btn_six.grid(row=2, column=2)
-btn_minus.grid(row=2, column=3)
-btn_one.grid(row=3, column=0)
-btn_two.grid(row=3, column=1)
-btn_three.grid(row=3, column=2)
-btn_multiply.grid(row=3, column=3)
-btn_dot.grid(row=4, column=0)
-btn_zero.grid(row=4, column=1)
-btn_clear.grid(row=4, column=2)
-btn_equal.grid(row=4, column=3)
-
-
+for i, calc_key_obj in enumerate(calc_keys_objs):
+    calc_key_obj.grid(row=(i//4)+1, column=i%4, sticky='nsew')
 
 
 window.mainloop()
